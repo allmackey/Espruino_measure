174e737eabc3ba371f9573cdc22518a9a41695ad
allmackey <Allen.Mackey@gmail.com>
2020-01-08 22:14:27 -0500

    Update APDS9960.js

--- APDS9960.js
+++ APDS9960.js
@@ -82,8 +82,8 @@ GIEN           : 0       // Disable gesture interrupts
 function APDS9960(i2c) {
   this.addr = 0x39;
   this.i2c = i2c;
-  print("V0");
-  if (this.r(R.ID)!=57) throw "APDS9960 not found ("+this.r(R.ID)+")";
+  print("V1");
+  if (this.r(R.ID)!=0xAB) throw "APDS9960 not found ("+this.r(R.ID)+")";
   // disable everything initially
   this.w(R.ENABLE, 0);
   /* Set default values for ambient light and proximity registers */
